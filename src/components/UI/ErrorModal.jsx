d772c4eef06c5468958b8e2339022a73319602f9
Harsh Vaghani <harshvaghani@ET3MedalliaMac297s-MacBook-Air.local>
2022-12-18 01:37:53 +0530

    Adding Portals

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,25 +1,53 @@
 import Card from "./Card";
 import Button from "./Button";
 import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+/**
+ * React Portals are used to render elements like Overlays, Modals at a different position in the Dom rather than inside root.
+ * This helps to keep the code structure clean.
+ * The react-dom package is imported and the create portal method is made use of.
+ * The create portal method takes two arguments.
+ * First is the component that needs to be rendered.
+ * Second is the location where it has to be rendered. The location can be obtained by using DOM getElementById method.
+ */
+
+const BackDrop = ({ onConfirm }) => {
+  return <div className="backdrop" onClick={onConfirm}></div>;
+};
+
+const Modal = ({ error, onConfirm }) => {
+  return (
+    <Card className={"modalCard"}>
+      <header className={"modalHeader"}>
+        <h2>{error.title}</h2>
+      </header>
+      <section className="wrapper">
+        <section className={"modalContent"}>
+          <p>{error.message}</p>
+        </section>
+        <footer className={"modalFooter"}>
+          <Button className={"errorBtn"} onClick={onConfirm}>
+            Close
+          </Button>
+        </footer>
+      </section>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ error, onConfirm }) => {
   return (
     <Fragment>
-      <div className="backdrop" onClick={onConfirm}></div>
-      <Card className={"modalCard"}>
-        <header className={"modalHeader"}>
-          <h2>{error.title}</h2>
-        </header>
-        <section className="wrapper">
-          <section className={"modalContent"}>
-            <p>{error.message}</p>
-          </section>
-          <footer className={"modalFooter"}>
-            <Button className={"errorBtn"} onClick={onConfirm}>
-              Close
-            </Button>
-          </footer>
-        </section>
-      </Card>
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={onConfirm} />,
+        document.getElementById("backdrop-portal")
+      )}
+
+      {ReactDOM.createPortal(
+        <Modal error={error} onConfirm={onConfirm} />,
+        document.getElementById("modal-portal")
+      )}
     </Fragment>
   );
 };
